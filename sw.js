f27dd09b84c78ba862b8f6cd9d043fb52c4597be
Mohd Redzuan <152396475+mRedzuan451@users.noreply.github.com>
2025-08-02 22:49:30 +0800

    update

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'to-do-list-cache-v1';
+const urlsToCache = [
+  '/',
+  '/app.html',
+  '/app.js',
+  '/ui.js',
+  '/tasks.js',
+  '/storage.js',
+  '/calendar.js',
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css'
+];
+
+// Install a service worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Cache and return requests
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
